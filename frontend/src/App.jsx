0a696144c8b5f896b4ed0dc617bd67cffc12ffe3
RohansinhVihol <rohansinhvihol06@gmail.com>
2026-01-22 11:40:28 +0530

    Setup initial React structure for eCommerce frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Routes , Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import About from './pages/About'
+import Login from './pages/Login'
+import Product from './pages/Product'
+import PlaceOrder from './pages/PlaceOrder'
+import Contact from './pages/Contact'
+import Orders from './pages/Orders'
+
+
+const App = () => {
+  return (
+    <>
+  <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/collection' element = {<Collection/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path ='/product/:productId' element={<Product/>}/>
+      <Route path='/place-order' element={<PlaceOrder/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/orders' element={<Orders/>}/>
+    </Routes>
+    </div>
+    </>
+  )
+}
+
+export default App
