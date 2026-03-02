b6743846f1acaf95614f059adf6550b0af42ffa5
RohansinhVihol <rohansinhvihol06@gmail.com>
2026-03-02 12:06:45 +0530

    chore: update backend configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,20 @@
 import { app } from "./app.js";
+import fs from "fs"
 
 import dotenv from "dotenv";
 import { connectDB } from "./db/MongoDB.js";
+import connectCloudinary from "./config/cloudinary.js";
 dotenv.config();
 
 const port = process.env.PORT;
 
+connectCloudinary()
+
 connectDB().then(() => {
     app.listen(port, () => {
-    console.log("server running on a " + port)
+        console.log("server running on a " + port)
 })
 }).catch((err) => {
     console.log("MongoDb connection failed",err.message)
 })
+
