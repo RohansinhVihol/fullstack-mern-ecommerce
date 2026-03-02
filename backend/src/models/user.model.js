78a9f5c814568204906497f14cffa0e270099448
RohansinhVihol <rohansinhvihol06@gmail.com>
2026-03-02 11:53:55 +0530

    feat: add bcrypt and JWT authentication in user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -6,5 +8,32 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
     cartData: { type: Object, default: {} }
 }, { minimize: false }) //using this minimize properties we can also save empty object in database
+
+userSchema.pre("save", async function(){
+    try {
+        if (!this.isModified("password")) return ;
+        this.password = await bcrypt.hash(this.password,10);
+        
+    } catch (error) {
+        
+    }
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateJwtToken = function(){
+    return jwt.sign({
+        id:this._id,
+        email:this.email,
+    },
+    process.env.JWT_TOKEN_SECRET,
+    {
+        expiresIn:process.env.JWT_TOKEN_EXPIRE
+    })
+}
+
+
 
 export const User = mongoose.model('User',userSchema)
